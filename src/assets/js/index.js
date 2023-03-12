1a8d6bba50e6a4b5d8c9f3b515a9d235f233b535
Илья Ерыкалин <Erykalin1986@yandex.ru>
2023-03-12 16:39:45 +0300

    added initSlider() function and refactored code

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,23 +1,25 @@
-import '../styles/reset.scss';
-import '../styles/mixins.scss';
-import '../styles/styles.scss';
+import "swiper/swiper.min.css";
+import "../styles/reset.scss";
+import "../styles/styles.scss";
+import Swiper, { Navigation } from "swiper";
+Swiper.use([Navigation]);
 
 let isPlay = false;
 const checkboxes = {
-    requirements: ['minimum', 'recommended'],
-    versions: ['standard', 'limited']
-}
+  requirements: ["minimum", "recommended"],
+  versions: ["standard", "limited"],
+};
 const classes = {
-    opened: 'opened',
-    hidden: 'hidden',
-    active: 'active'
-}
+  opened: "opened",
+  hidden: "hidden",
+  active: "active",
+};
 
-const checkbox = document.querySelectorAll('.checkbox');
-const header = document.querySelector('.header');
-const menuLink = document.querySelectorAll('.menu-link');
-const menuButton = document.querySelector('.header-menu__button');
-const video = document.getElementById('video');
+const checkbox = document.querySelectorAll(".checkbox");
+const header = document.querySelector(".header");
+const menuLink = document.querySelectorAll(".menu-link");
+const menuButton = document.querySelector(".header-menu__button");
+const video = document.getElementById("video");
 const videoButton = document.querySelector(".video-btn");
 
 /**
@@ -32,22 +34,22 @@ const toggleMenu = () => header.classList.toggle(classes.opened);
  * @param {*} event Событие click.
  */
 const scrollToSection = (event) => {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute("href");
 
-    if (!href && !href.startsWith('#')) return;
+  if (!href && !href.startsWith("#")) return;
 
-    const section = href.slice(1);
-    const top = document.getElementById(section)?.offsetTop || 0;
-    window.scrollTo({ top, behavior: 'smooth'});
-}
+  const section = href.slice(1);
+  const top = document.getElementById(section)?.offsetTop || 0;
+  window.scrollTo({ top, behavior: "smooth" });
+};
 
 /**
  * Форматирует значение таймера.
  * @param {*} value Значение таймера.
  * @returns Занчение таймера в определённом формате.
  */
-const formatValue = (value) => value < 10 ? `0${value}` : value;
+const formatValue = (value) => (value < 10 ? `0${value}` : value);
 
 /**
  * Получает значения разницы даты для таймера.
@@ -55,76 +57,93 @@ const formatValue = (value) => value < 10 ? `0${value}` : value;
  * @returns Объект содержащий в себе значения для таймера.
  */
 const getTimerValues = (diff) => {
-    return {
-        seconds: (diff / 1000) % 60,
-        minutes: (diff / (1000 * 60)) % 60,
-        hours: (diff / (1000 * 3600)) % 24,
-        days: (diff / (1000 * 3600 * 24)) % 30
-    }
-}
+  return {
+    seconds: (diff / 1000) % 60,
+    minutes: (diff / (1000 * 60)) % 60,
+    hours: (diff / (1000 * 3600)) % 24,
+    days: (diff / (1000 * 3600 * 24)) % 30,
+  };
+};
 
 /**
  * Заполняет значения таймера.
  * @param {*} values Значения таймера.
  */
 const setTimerValues = (values) => {
-    Object.entries(values).forEach(([key, value]) => {
-        const timerValue = document.getElementById(key);
-        timerValue.innerText = formatValue(Math.floor(value));
-    });
-}
+  Object.entries(values).forEach(([key, value]) => {
+    const timerValue = document.getElementById(key);
+    timerValue.innerText = formatValue(Math.floor(value));
+  });
+};
 
 /**
  * Запускает таймер.
  * @param {*} date Конечная дата.
  */
 const startTimer = (date) => {
-    const intervalId = setInterval(() => {
-        const diff = new Date(date).getTime() - new Date().getTime();
+  const intervalId = setInterval(() => {
+    const diff = new Date(date).getTime() - new Date().getTime();
 
-        if (diff < 0) {
-            clearInterval(intervalId);
-            return;
-        }
+    if (diff < 0) {
+      clearInterval(intervalId);
+      return;
+    }
 
-        setTimerValues(getTimerValues(diff));
-    }, 1000);
-}
+    setTimerValues(getTimerValues(diff));
+  }, 1000);
+};
 
 /**
  * Запускает или ставит на паузу воспроизведение видео.
  * @param {*} param0 Элемент кнопки.
  */
 const handleVideo = ({ target }) => {
-    const info = target.parentElement;
-    isPlay = !isPlay;
-    info.classList.toggle(classes.hidden, isPlay);
-    target.innerText = isPlay ? 'Pause' : 'Play';
-    isPlay ? video.play() : video.pause();
-}
+  const info = target.parentElement;
+  isPlay = !isPlay;
+  info.classList.toggle(classes.hidden, isPlay);
+  target.innerText = isPlay ? "Pause" : "Play";
+  isPlay ? video.play() : video.pause();
+};
 
 /**
  * Переключает описание по клику.
  * @param {*} param0 checked - положение переключателя. name - название блока.
  */
 const handleCheckbox = ({ currentTarget: { checked, name } }) => {
-    const { active } = classes;
-    const value = checkboxes[name][Number(checked)];
-    const list = document.getElementById(value);
-    const tabs = document.querySelectorAll(`[data-${name}]`);
-    const siblings = list.parentElement.children;
+  const { active } = classes;
+  const value = checkboxes[name][Number(checked)];
+  const list = document.getElementById(value);
+  const tabs = document.querySelectorAll(`[data-${name}]`);
+  const siblings = list.parentElement.children;
 
-    for (const item of siblings) item.classList.remove(active);
-    for (const tab of tabs) {
-        tab.classList.remove(active);
-        tab.dataset[name] === value && tab.classList.add(active);
-    }
+  for (const item of siblings) item.classList.remove(active);
+  for (const tab of tabs) {
+    tab.classList.remove(active);
+    tab.dataset[name] === value && tab.classList.add(active);
+  }
 
-    list.classList.add(active);
-}
+  list.classList.add(active);
+};
 
-startTimer('November 11, 2023 00:00:00');
-menuButton.addEventListener('click', toggleMenu);
-videoButton.addEventListener('click', handleVideo);
-menuLink.forEach((link) => link.addEventListener('click', scrollToSection));
-checkbox.forEach((box) => box.addEventListener('click', handleCheckbox));
+/**
+ * Инициализация слайдера.
+ */
+const initSlider = () => {
+  new Swiper(".swiper", {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    initialSlide: 2,
+    navigation: {
+      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-next",
+    },
+  });
+};
+
+initSlider();
+startTimer("November 11, 2023 00:00:00");
+menuButton.addEventListener("click", toggleMenu);
+videoButton.addEventListener("click", handleVideo);
+menuLink.forEach((link) => link.addEventListener("click", scrollToSection));
+checkbox.forEach((box) => box.addEventListener("click", handleCheckbox));
